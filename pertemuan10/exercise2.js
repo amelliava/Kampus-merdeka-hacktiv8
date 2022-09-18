756a92156e5055dfa0efbd3e0fbbe22f6b1edb59
amelliava <2010631250004@student.unsika.ac.id>
2022-09-18 20:48:20 +0700

    pert10

---
+++ pertemuan10/exercise2.js
@@ -0,0 +1,10 @@
+const biodata = {
+    nama_depan: 'amel',
+    nama_belakang: 'veronica',
+    hobby: ["membaca", "nyanyi", "travelling", "belajar", "drakoran"],
+    angka_favorit: 7,
+    memakai_kacamata: true,
+    status: 'belum menikah'
+}
+
+console.log(biodata);
